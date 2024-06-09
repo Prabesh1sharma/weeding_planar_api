902323ea5bd72577e962f87148cbb394c110708c
Prabesh Sharma <sharmaprabesh2027@gmail.com>
2024-06-09 16:50:04 +0545

    fix

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,4 @@
 echo "BUILD START"
-python -m venv env
-source env/bin/activate
 python -m pip install -r requirements.txt
 python manage.py collectstatic --noinput --clear
 mkdir -p staticfiles_build
